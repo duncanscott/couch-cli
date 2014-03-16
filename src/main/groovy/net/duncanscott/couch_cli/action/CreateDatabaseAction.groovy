8e0c9089e1063e4c7ae9a1b929701f25334ce8d7
Duncan <dnscott@gmail.com>
2014-03-15 17:22:24 -0700

    in progress

---
+++ src/main/groovy/net/duncanscott/couch_cli/action/CreateDatabaseAction.groovy
@@ -0,0 +1,23 @@
+package net.duncanscott.couch_cli.action
+
+class CreateDatabaseAction extends AbstractAction {
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int performAction() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void configureOptions() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
