2f92e4bb69c579fb71e3e6b7c0b09ff76740a7bf
Duncan <dnscott@gmail.com>
2014-03-16 02:40:41 -0700

    added CreateDatabseAction, not tested refactored DeleteDatabaseAction, not tested

--- src/main/groovy/net/duncanscott/couch_cli/action/CreateDatabaseAction.groovy
+++ src/main/groovy/net/duncanscott/couch_cli/action/CreateDatabaseAction.groovy
@@ -1,23 +1,36 @@
 package net.duncanscott.couch_cli.action
 
+import groovy.util.ConfigObject;
+import net.duncanscott.couch_cli.option.NameOption
+
+import org.apache.commons.cli.Option
+
 class CreateDatabaseAction extends AbstractAction {
 
+	NameOption nameOption
+	
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		'Create database(s) at the specified couchdb.  New databases are named via the --name option.'
+	}
+
+	void createDatabase(ConfigObject couchDb, String name) {
+		api.createDatabase(couchDb.couchdb.url, name)
+		message "created database ${couchDb.couchdb.name}.${name}"
+	}
+	
+	@Override
+	public void configureOptions() {
+		nameOption = new NameOption(this)
 	}
 
 	@Override
 	public int performAction() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void configureOptions() {
-		// TODO Auto-generated method stub
-		
+		ConfigObject couchDb = requireSubjectDatabase()
+		nameOption.names.each { String name ->
+			createDatabase(couchDb,name)
+		}
+		return CouchClient.SUCCESS
 	}
 
 }
